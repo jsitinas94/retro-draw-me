b929a06392a28218c352cf9021ced8f0870addc5
jsitinas94 <jsitinas94@gmail.com>
2021-08-07 11:48:44 -0400

    added functionality to action panel

--- src/components/ActionPanel.js
+++ src/components/ActionPanel.js
@@ -7,7 +7,7 @@ import { buildCellList } from '../utils';
  * The ActionPanel component represents the interface for updating the Grid
  * based on any number of buttons the user might click on.
  */
-const ActionPanel = (props) => {
+const ActionPanel = ({activeColor, cellList, setCellList}) => {
   /**
    * Create constants for activeColor, cellList, and setCellList, reading them from the props
    */
@@ -17,15 +17,23 @@ const ActionPanel = (props) => {
       This button needs an onClick function which:
         - creates a new cell list using buildCellList
         - passes the new cell list to setCellList
-    */}
-    <button>clear all</button>
+    */
+    }
+    <button onClick={() => {
+      const emptyCellList = buildCellList();
+      setCellList(emptyCellList)
+    }}>clear all</button>
     {/* 
       This button needs an onClick function which:
         - creates a new cell list using buildCellList
         - loops over it, setting the color on each cell to activeColor
         - passes the new cell list to setCellList
     */}
-    <button>fill all</button>
+    <button onClick={() => {
+      const fillAllCellList = buildCellList();
+      fillAllCellList.map((cell) => cell.color=activeColor)
+      setCellList(fillAllCellList)
+    }}>fill all</button>
     {/* 
       This buttonm needs an onClick function which:
         - creates a new cell list using buildCellList
@@ -33,7 +41,17 @@ const ActionPanel = (props) => {
           - set the corresponding (by index) new cell to its color (if it has one) OR
           - set the corresponding (by index) new cell to the activeColor
     */}
-    <button>fill empty</button>
+    <button onClick={() => {
+      const fillEmptyCellList = buildCellList();
+      cellList.map((cell, index) => {
+        if(cell.color === null) {
+          fillEmptyCellList[index].color = activeColor
+        } else {
+           fillEmptyCellList[index].color = cell.color
+        }
+      })
+      setCellList(fillEmptyCellList)
+    }}>fill empty</button>
   </div>
 }
 
